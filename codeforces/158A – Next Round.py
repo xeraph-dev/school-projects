5793f0b70a5a666b1eb7cf5ed3bbecd54efe8ff7
Xeraph <61700204+xeraph-dev@users.noreply.github.com>
2025-09-29 12:03:16 -0400

    feat(codeforces): solve 158A – Next Round in 3 lines

--- codeforces/158A – Next Round.py
+++ codeforces/158A – Next Round.py
@@ -1,13 +1,17 @@
-line = input()
-line_split = line.split(" ")
+# line = input()
+# line_split = line.split(" ")
 
-count = int(line_split[0])
-idx = int(line_split[1]) - 1
+# count = int(line_split[0])
+# idx = int(line_split[1]) - 1
 
-line = input()
-scores = [int(score) for score in line.split(" ")]
-min_score = scores[idx]
+# line = input()
+# scores = [int(score) for score in line.split(" ")]
+# min_score = scores[idx]
 
-result = sum(1 for score in scores if 0 < score >= min_score)
+# result = sum(1 for score in scores if 0 < score >= min_score)
 
-print(result)
+# print(result)
+
+idx = int(input().split(" ")[1]) - 1
+scores = [int(score) for score in input().split(" ")]
+print(sum(1 for score in scores if 0 < score >= scores[idx]))
